c6a40e0cb52ea2b8313a521931379bc6ac2810fd
Toke Faurby <Tokeee@gmail.com>
2016-07-08 08:33:49 +0200

    Initial commit

---
+++ assignments/ass_1.py
@@ -0,0 +1,30 @@
+
+# from __future__ import print_function
+	# Bring the print function from python 3 into python 2.6+
+import os
+import sys
+import tarfile
+import numpy as np
+import matplotlib.pyplot as plt
+
+from scipy import ndimage
+from sklearn.linear_model import LogisticRegression
+from six.moves.urllib.request import urlretrieve
+from six.moves import cPickle as pickle
+
+# Configure the matplotlib backend as plotting inline in IPython
+# %matplotlib inline
+
+
+
+
+
+
+
+
+
+
+
+
+
+
