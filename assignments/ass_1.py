47b92f38523df23b202cb0ceaea4f7c4c39a4843
Toke Faurby <Tokeee@gmail.com>
2016-07-08 11:04:59 +0200

    Remove unused library imports

--- assignments/ass_1.py
+++ assignments/ass_1.py
@@ -1,20 +1,4 @@
-
-# from __future__ import print_function
-	# Bring the print function from python 3 into python 2.6+
-import os
-import sys
-import tarfile
-import numpy as np
-import matplotlib.pyplot as plt
-
-from scipy import ndimage
-# from sklearn.linear_model import LogisticRegression
-from six.moves import cPickle as pickle
-
 from tools import *
-
-# Configure the matplotlib backend as plotting inline in IPython
-# %matplotlib inline
 
 
 print('Aquiring dataset ...')
